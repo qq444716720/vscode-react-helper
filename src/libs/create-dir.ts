97269843aa9bb64955ca116368b6011a40950e62
wb-cs555011 <wb-cs555011@koubei.com>
2019-06-17 15:06:50 +0800

    update

--- src/libs/create-dir.ts
+++ src/libs/create-dir.ts
@@ -8,8 +8,8 @@ const upperCamelCase = require('uppercamelcase');
  * @param name 组件名称
  */
 export const createDir = (dirPath: string, name: string): string => {
-  const componentName = upperCamelCase(name);
-  const componentDirPath = path.resolve(dirPath, componentName);
+  // const componentName = upperCamelCase(name);
+  const componentDirPath = path.resolve(dirPath, name);
   let isComponentExit = fs.existsSync(componentDirPath);
   if (isComponentExit) {
     return void toast.error(`该目录:${componentDirPath}已存在，无法继续创建组件。`)
